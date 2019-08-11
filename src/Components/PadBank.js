0e7049b0944ffd566a45d9ba5b7e358f644cc930
Michael Brünen <mb156208@fh-muenster.de>
2019-08-12 00:34:18 +0200

    Finish PadBank component

--- src/Components/PadBank.js
+++ src/Components/PadBank.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react';
 
-class PadBank extends Component {
+import DrumPad from './DrumPad';
 
+class PadBank extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    let padBank;
+    if (this.props.power) { // if drum machine is on
+      // return Drumpad Component with active clips, map the currentPadBank
+      // also hand over power state and updateDisplay function
+      padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+        return (
+          <DrumPad
+            clip={padBankArr[i].url}
+            clipId={padBankArr[i].id}
+            keyCode={padBankArr[i].keyCode}
+            keyTrigger={padBankArr[i].keyTrigger}
+            power={this.props.power}
+            updateDisplay={this.props.updateDisplay}
+          />
+        );
+      });
+    } else {
+      // same as above, but no active clips without power, so that no sound will play when clicking
+      padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+        return (
+          <DrumPad
+            clip='#'
+            clipId={padBankArr[i].id}
+            keyCode={padBankArr[i].keyCode}
+            keyTrigger={padBankArr[i].keyTrigger}
+            power={this.props.power}
+            updateDisplay={this.props.updateDisplay}
+          />
+        );
+      });
+    }
+
+    return (
+      <div className='pad-bank'>{padBank}</div>
+    )
+  }
 }
 
 export default PadBank;
